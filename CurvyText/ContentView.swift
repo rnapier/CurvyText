dd028fc2041ea8c2a87d43792c176f664650c5e7
Rob Napier <rob@neverwood.org>
2019-12-06 10:01:04 -0500

    Initial Commit

---
+++ CurvyText/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CurvyText
+//
+//  Created by Rob Napier on 12/6/19.
+//  Copyright © 2019 Rob Napier. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
