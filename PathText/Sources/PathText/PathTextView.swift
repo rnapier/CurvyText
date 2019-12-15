ffe922a22b663b67bdf0a1e6317f4aaed22e0fb8
Rob Napier <rob@neverwood.org>
2019-12-15 17:12:12 -0500

    Hardcoded baseline adjustment

--- PathText/Sources/PathText/PathTextView.swift
+++ PathText/Sources/PathText/PathTextView.swift
@@ -25,7 +25,12 @@ public struct PathText {
         set {
             textStorage.setAttributedString(newValue)
             locations = (0..<layoutManager.numberOfGlyphs).map { [layoutManager] glyphIndex in
-                layoutManager.location(forGlyphAt: glyphIndex)
+                return layoutManager.boundingRect(forGlyphRange: NSRange(location: glyphIndex, length: 1),
+                                                  in: textContainer)
+//                let leadingBottom = layoutManager.location(forGlyphAt: glyphIndex).x
+//                let rect = layoutManager.boundingRect(forGlyphRange: NSRange(location: glyphIndex, length: 1),
+//                                                      in: textContainer)
+//                return leadingBottom + rect.size.width / 2
             }
 
             lineFragmentOrigin = layoutManager
@@ -39,7 +44,7 @@ public struct PathText {
     private let textStorage = NSTextStorage()
     private let textContainer = NSTextContainer()
 
-    private var locations: [CGPoint] = []
+    private var locations: [CGRect] = []
     private var lineFragmentOrigin = CGPoint.zero
 
     public init(text: NSAttributedString, path: Path) {
@@ -56,7 +61,7 @@ public struct PathText {
 extension PathText: View {
     public var body: some View {
 
-        let tangents = path.getTangents(atLocations: locations.map { $0.x })
+        let tangents = path.getTangents(atLocations: locations.map {$0.origin.x})
 
         var strings: [String] = [] // FIXME: Will be NSAttributedString
         var glyphRange = NSRange(location: 0, length: 1)
@@ -72,152 +77,37 @@ extension PathText: View {
             let angle: Double
             let point: CGPoint
             let string: String
+            let location: CGRect
         }
 
-        let runs = zip(tangents, strings).map { tangent, string in
-            Run(angle: Double(tangent.angle), point: tangent.point, string: string)
+        var runs: [Run] = []
+        for (tangent, (string, location)) in zip(tangents, zip(strings, locations)) {
+            runs.append(Run(angle: Double(tangent.angle), point: tangent.point, string: string, location: location))
         }
 
         // FIXME: include lineFragmentOrigin (but currently it's .zero)
 
-        return ZStack(alignment: .topLeading) {
-            Rectangle().foregroundColor(.clear) // FIXME: Making Zstack size of containing view. Easier way?
+        let baselineShift: CGFloat = 10.0
+
+        return ZStack {
             ForEach(runs) { run in
-//                ZStack(alignment: .topLeading) {
-                    Text(verbatim: run.string)
-                        .font(.system(size: 48))
-                        .border(Color.green)
-                        .rotationEffect(.radians(run.angle), anchor: .bottomLeading)
-                        .offset(x: run.point.x, y: run.point.y)
-                    .alignmentGuide(.top) { d in d[.lastTextBaseline] }
-                    //                    .offset(x: 10, y: 10)
+                Text(verbatim: run.string)
+                    .font(.system(size: 48))
+                    .padding(EdgeInsets(top: baselineShift, leading: 0, bottom: -baselineShift, trailing: 0))
+                    .border(Color.green)
+                    .rotationEffect(.radians(run.angle), anchor: .bottomLeading)
+                    .offset(x: run.location.width / 2, y: (-run.location.height / 2))
+                    .position(run.point)
                 Circle()
                     .foregroundColor(.red)
                     .frame(width: 5, height: 5)
                     .position(run.point)
-                }
-        }.border(Color.red)
-//        }
-//        for (index, (tangent, location)) in zip(tangents, locations).enumerated() {
-//
-//
-//            let glyphPoint = tangent.point
-//            let angle = tangent.angle
-//
-//
-//            context.translateBy(x: glyphPoint.x, y: glyphPoint.y)
-//            context.rotate(by: angle)
-//
-//            // The "at:" in drawGlyphs is the origin of the line fragment. We've already adjusted the
-//            // context, so take that back out.
-//            let adjustedOrigin = CGPoint(x: -(lineFragmentOrigin.x + location.x),
-//                                         y: -(lineFragmentOrigin.y + location.y))
-//
-//            layoutManager.drawGlyphs(forGlyphRange: NSRange(location: index, length: 1),
-//                                     at: adjustedOrigin)
-//
-//            context.restoreGState()
-//        }
-//
-//
-//
-//        Text("Stuff")
+            }
+        }
+        .border(Color.red)
     }
 }
 
-
-//@available(iOS 13.0, *)
-//public struct PathText: UIViewRepresentable {
-//    public var text: NSAttributedString
-//    public var path: CGPath
-//
-//    public init(text: NSAttributedString, path: Path) {
-//        self.init(text: text, path: path.cgPath)
-//    }
-//
-//    public init(text: NSAttributedString, path: CGPath) {
-//        self.text = text
-//        self.path = path
-//    }
-//
-//    public func makeUIView(context: UIViewRepresentableContext<PathText>) -> PathTextView {
-//        PathTextView()
-//    }
-//
-//    public func updateUIView(_ uiView: PathTextView, context: UIViewRepresentableContext<PathText>) {
-//        uiView.text = text
-//        uiView.path = path
-//    }
-//}
-
-/*
- Draws attributed text along a cubic Bezier path defined by P0, P1, P2, and P3
- */
-//public class PathTextView: UIView {
-//
-//    public var path: CGPath = CGMutablePath() {
-//        didSet {
-//            setNeedsDisplay()
-//        }
-//    }
-//
-//    public var text: NSAttributedString {
-//        get { textStorage }
-//        set {
-//            textStorage.setAttributedString(newValue)
-//            locations = (0..<layoutManager.numberOfGlyphs).map { [layoutManager] glyphIndex in
-//                layoutManager.location(forGlyphAt: glyphIndex)
-//            }
-//
-//            lineFragmentOrigin = layoutManager
-//                .lineFragmentRect(forGlyphAt: 0, effectiveRange: nil)
-//                .origin
-//        }
-//    }
-//
-//    private let layoutManager = NSLayoutManager()
-//    private let textStorage = NSTextStorage()
-//    private let textContainer = NSTextContainer()
-//
-//    private var locations: [CGPoint] = []
-//    private var lineFragmentOrigin = CGPoint.zero
-//
-//    public init() {
-//        layoutManager.addTextContainer(textContainer)
-//        textStorage.addLayoutManager(layoutManager)
-//        super.init(frame: .zero)
-//        backgroundColor = .clear
-//    }
-//
-//    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
-//
-//    public override func draw(_ rect: CGRect) {
-//
-//        let tangents = path.getTangents(atLocations: locations.map { $0.x })
-//
-//        let context = UIGraphicsGetCurrentContext()!
-//
-//        for (index, (tangent, location)) in zip(tangents, locations).enumerated() {
-//            context.saveGState()
-//
-//            let glyphPoint = tangent.point
-//            let angle = tangent.angle
-//
-//            context.translateBy(x: glyphPoint.x, y: glyphPoint.y)
-//            context.rotate(by: angle)
-//
-//            // The "at:" in drawGlyphs is the origin of the line fragment. We've already adjusted the
-//            // context, so take that back out.
-//            let adjustedOrigin = CGPoint(x: -(lineFragmentOrigin.x + location.x),
-//                                         y: -(lineFragmentOrigin.y + location.y))
-//
-//            layoutManager.drawGlyphs(forGlyphRange: NSRange(location: index, length: 1),
-//                                     at: adjustedOrigin)
-//
-//            context.restoreGState()
-//        }
-//    }
-//}
 
 @available(iOS 13.0.0, *)
 struct PathText_Previews: PreviewProvider {
@@ -259,8 +149,15 @@ struct PathText_Previews: PreviewProvider {
             $0.addLine(to: P1)
         }
 
-        return ZStack {PathText(text: text, path: path)
-            path.stroke(Color.blue, lineWidth: 2)
+        return VStack {
+            Text("You can display text along a curve")
+                .font(.system(size: 48))
+                .padding(.horizontal)
+                .lineLimit(1)
+            ZStack {
+                PathText(text: text, path: path)
+                path.stroke(Color.blue, lineWidth: 2)
+            }
         }
     }
 
